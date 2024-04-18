b49309ae239fe0640c7bf72902305c8bab548cbb
Cristiano R. Siebert <cristiano.siebert@trust.group>
2024-04-18 13:54:14 -0300

    feat: permitindo executar o mapeamento de forma programática

--- src/index.js
+++ src/index.js
@@ -32,4 +32,36 @@ function prepareClassMappings(classMappings) {
     });
 }
 
-export { prepareClassMappings };
+function executeClassMapping(classMappings) {
+    let classList = [];
+    const resolveClass = (cls) => {
+        // Evita loop infinito e repetição de classes
+        if (classList.includes(cls)) {
+            return [];
+        }
+        // Se a classe tem um alias ele resolve, senão ele adiciona a classe à lista.
+        if (classMappings[cls]) {
+            classList.push(cls);
+            const classes = classMappings[cls].split(/\s+/);
+            let resolvedClasses = [];
+            classes.forEach(cls => {
+                resolvedClasses = resolvedClasses.concat(resolveClass(cls));
+            });
+            return resolvedClasses;
+        } else {
+            classList.push(cls);
+            return [cls];
+        }
+    }
+    document.querySelectorAll('[class]').forEach(el => {
+        classList = [];
+        const classes = el.className.split(/\s+/);
+        let resolvedClasses = [];
+        classes.forEach(cls => {
+            resolvedClasses = resolvedClasses.concat(resolveClass(cls));
+        });
+        el.className = resolvedClasses.join(' ');
+    });
+}
+
+export { prepareClassMappings, executeClassMapping };
